4416e62cc2a02a3e6b63a40f4842e647f51defdc
Pedro Henrique Cabral Silva <marialeitaoautora@gmail.com>
2022-12-30 13:12:06 -0300

    50.DateTime2

---
+++ 50.DateTime2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _50.DateTime2 {
+    class Program {
+        static void Main(string[] args) {
+
+            DateTime dataNascimento = new DateTime(1996, 02, 15);
+
+            DateTime dataAtual = DateTime.Now;
+
+            //TimeSpan é uma quantidade de tempo. Qualquer operação feita com DateTime retorna um TimeSpan.
+            TimeSpan diferenca = dataAtual - dataNascimento;
+
+            //int idade = Convert.ToInt32(Math.Floor(diferenca.TotalDays / 365.0));
+
+            int idade = (int)(diferenca.TotalDays / 365.0);
+
+            Console.WriteLine(idade);
+
+        }
+    }
+}
